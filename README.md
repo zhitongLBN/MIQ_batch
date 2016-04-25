70739f60881011a0c015ed6258e8223fd01cd009
zhitongLBN <z.liu@linkbynet.com>
2016-04-25 17:20:58 +0200

    add condition to execute modifies for ovh

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MIQ_batch
+Execute this scripts in your ManageIQ folder. It will enable your MIQ's openstack use *ovh SBG1*
--- patch_openstack_ovh_SBG1.sh
+++ patch_openstack_ovh_SBG1.sh
@@ -8,13 +8,16 @@ refresh_parser_line_to_comment=(
   '      get_availability_zones'
   '      load_orchestration_stacks'
 )
-
+line_if='      unless @os_handle.address.include? "cloud.ovh.net"'
+line_end='      end'
 for line in "${refresh_parser_line_to_comment[@]}"
 do
-  sed -i "s/$line/# $line/g" $refresh_parser
+  sed -i "s/$line/$line_if\n\ \ $line\n$line_end/g" $refresh_parser
 done
 
 openstack_handler='gems/pending/openstack/openstack_handle/handle.rb'
-aim_line=":openstack_endpoint_type => 'publicURL',"
-line_to_append="        :openstack_region => 'SBG1'"
-sed -i "s/$aim_line/$aim_line\n$line_to_append/g" $openstack_handler
+aim_line="      opts.merge!(extra_opts) if extra_opts"
+line1="      if auth_url\.include\? \"cloud\.ovh\.net\""
+line2="        opts[:openstack_region] = 'SBG1'"
+line3="      end"
+sed -i "s/$aim_line/$aim_line\n$line1\n$line2\n$line3/g" $openstack_handler
