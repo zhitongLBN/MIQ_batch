783426848ab05c682b2d186f8c4222d393cbe236
zhitong liu <z.liu@linkbynet.com>
2017-04-07 12:48:09 +0200

    Replace git reset by git checkout for re-apply the patch

--- patch_openstack_ovh_SBG1.sh
+++ patch_openstack_ovh_SBG1.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-git reset --hard HEAD
+# List of files
+refresh_parser='app/models/manageiq/providers/openstack/cloud_manager/refresh_parser.rb'
+cloud_manager='app/models/manageiq/providers/openstack/cloud_manager.rb'
+ampq_event_monitor='gems/pending/openstack/events/openstack_rabbit_event_monitor.rb'
+cloudmanager_vm='app/models/manageiq/providers/cloud_manager/vm.rb'
+manager_mixin='app/models/manageiq/providers/openstack/manager_mixin.rb'
+
+git checkout $refresh_parser $cloud_manager $ampq_event_monitor $cloudmanager_vm $manager_mixin
 
 # comment service not exist in ovh
-refresh_parser='app/models/manageiq/providers/openstack/cloud_manager/refresh_parser.rb'
+# in $refresh_parser
 refresh_parser_line_to_comment=(
   '      @orchestration_service      = @os_handle.detect_orchestration_service'
   '      @nfv_service                = @os_handle.detect_nfv_service'
@@ -20,7 +27,7 @@ do
 done
 
 # disable test_conntection for amqp for ovh
-ampq_event_monitor='gems/pending/openstack/events/openstack_rabbit_event_monitor.rb'
+# in $ampq_event_monitor
 aim_line='      connection.start'
 line_if_amqp='      unless options[:hostname].include? "cloud.ovh.net"'
 line_end_amqp='      end'
@@ -29,7 +36,7 @@ sed -i "s/$aim_line/$line_if_amqp\n  $aim_line\n$line_end_amqp/g" $ampq_event_mo
 # get public network address of ovh vm
 # should be delete if this is solved
 # https://github.com/fog/fog-openstack/issues/39
-refresh_parser='app/models/manageiq/providers/openstack/cloud_manager/refresh_parser.rb'
+# in $refresh_parser
 aim_line='      public_network  = {:ipaddress => server.public_ip_address}.delete_nils'
 
 line_to_add="      if @os_handle.address.include? \"cloud.ovh.net\"
@@ -46,14 +53,14 @@ sed -i "/$aim_line/r miq_patch_tmp_text" $refresh_parser
 rm miq_patch_tmp_text
 
 # disable uniqueness hostname for ovh
-cloud_manager='app/models/manageiq/providers/openstack/cloud_manager.rb'
+# in $cloud_manager
 aim_line='    return unless hostname.present? # Presence is checked elsewhere'
 line_to_add='    return if hostname.include? "cloud.ovh.net"'
 sed -i "s/$aim_line/$aim_line\n$line_to_add/" $cloud_manager
 
-cloudmanager_vm='app/models/manageiq/providers/cloud_manager/vm.rb'
+# modify the way to get ip from ovh
+# in $cloudmanager_vm
 aim_line='    @ipaddresses ||= network_ports.collect(&:ipaddresses).flatten.compact.uniq'
-
 line_to_add="    @ipaddresses = hardware.networks.collect(&:ipaddress).compact.uniq if @ipaddress.nil? || @ipaddresses.empty?"
 
 echo "$line_to_add" > miq_patch_tmp_text
@@ -61,10 +68,7 @@ sed -i "/$aim_line/r miq_patch_tmp_text" $cloudmanager_vm
 rm miq_patch_tmp_text
 
 # disable meter event monitor for ovh
-manager_mixin='app/models/manageiq/providers/openstack/manager_mixin.rb'
+# in $manager_mixin
 aim_line='  OpenstackEventMonitor.available?(event_monitor_options)'
 line_to_add='  return false if hostname.include? "cloud.ovh.net"'
 sed -i "s/$aim_line/$line_to_add\n  $aim_line/g" $manager_mixin
-
-
-
